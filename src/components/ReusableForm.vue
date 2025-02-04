c8e0009ea211bc5cab16c1dc48764c915d3a3f5d
MartinezJ10 <jl.mendoza@unah.hn>
2025-02-03 19:25:51 -0600

    Templating the login and making reusable form component

---
+++ src/components/ReusableForm.vue
@@ -0,0 +1,102 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <div v-for="(field, index) in fields" :key="index" class="form-group">
+            <label :for="field.name">{{ field.label }}</label>
+            
+           
+            <input v-if="field.type === 'text'" v-model="formData[field.name]" :type="field.type" :name="field.name"
+            :id="field.name" />
+           
+            
+            <input v-if="field.type === 'password'" v-model="formData[field.name]" :type="field.type" :name="field.name"
+            :id="field.name" />
+
+            <input v-if="field.type === 'email'" v-model="formData[field.name]" :type="field.type" :name="field.name"
+            :id="field.name" />
+
+
+        </div>
+        <button type="submit">{{ submitButtonText }}</button>
+
+    </form>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    fields: {
+        type: Array,
+        required: true
+    },
+    submitButtonText: {
+        type: String,
+        default: 'Submit'
+    },
+    onSubmit: {
+        type: Function,
+        required: true
+    }
+});
+
+//Accumulator and field for each field in the array
+const formData = ref(props.fields.reduce((acc, field) => {
+    acc[field.name] = '';
+    return acc;
+}, {}));
+
+//Simply put the handleSubmit to the given function
+const handleSubmit = () => {
+ 
+    props.onSubmit(formData.value);
+};
+
+</script>
+
+<style scoped>
+    form{
+        display: grid;
+        grid-template-columns: 1fr;  /* Default to one column layout */
+        padding: 20px;
+    }
+    .form-group{
+        display: flex;
+        justify-items: left;
+        flex-direction: column;
+        margin: 10px 0;
+    }
+
+    label {
+    text-align: left;  
+    margin-bottom: 8px;
+    font-size: 20px;
+
+    }
+
+    input {
+        padding: 10px;
+        font-size: 16px;
+        border-radius: 20px;
+        border: 1.5px solid var(--obscure);
+        width: 100%;
+    }
+
+    button {
+        padding: 12px;
+        background-color: var(--main-blue);
+        color: white;
+        font-size: 18px;
+        border: 1.5px solid var(--main-blue);
+        border-radius: 20px;
+        cursor: pointer;
+        width: 50%;
+    }
+    button:hover {
+        background-color: white;
+        color: var(--main-blue);
+        border: 1.5px solid var(--main-blue);
+        transition: 0.5s ease;
+    }
+
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,19 +1,69 @@
 <template>
     <div class="login-view">
-        <h1>Login</h1>
-       
+        <div class="block-art">
+
+        </div>
+        <div class="block-form">
+            <h1>PROSENE</h1> 
+            <ReusableForm :fields="loginFields" submitButtonText="Acceder" :onSubmit="handleLoginSubmit" />
+        </div> 
     </div>
+    
 </template>
 
 <script>
-export default {
-    name: 'LoginView',
+    
+    import { ref } from "vue";
+    import ReusableForm from "../components/ReusableForm.vue";
 
-};
+    export default {
+        name: 'LoginView',
+        components: {
+            ReusableForm
+        },
+        setup() {
+            const loginFields = ref([
+                { name: 'email', label: 'Email', type: 'email' },
+                { name: 'password', label: 'Contraseña', type: 'password' }
+            ]);
+
+            //Handle the form submission
+            const handleLoginSubmit = (formData) => {
+                //console.log(formData);
+            };
+
+            //return everything haha
+            return {
+                loginFields,
+                handleLoginSubmit
+            };
+        }
+    };
+
 </script>
 
 <style scoped>
 .login-view {
-    /* Add your styles here */
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    width: 100%; 
+    height: 100vh;
 }
+.block-art{
+    background-color: var(--main-blue);
+}
+.block-form{
+    place-self: center;
+}
+
+@media (max-width: 600px) {
+    .login-view {
+        grid-template-columns: 1fr;
+    }   
+    .block-form{
+        margin: 2rem;
+    }
+
+}
+
 </style>
