094d7546fb67154bfb9719d5906389e2f784c892
aljosama23 <alejomartiz10@gmail.com>
2025-02-15 18:43:50 -0600

    Validaciones
    En ReusableForm.vue se agregaron las validaciones para que sean utilizables en cualquier formulario.

--- src/components/ReusableForm.vue
+++ src/components/ReusableForm.vue
@@ -3,7 +3,11 @@
         <div v-for="(field, index) in fields" :key="index" class="form-group">
             <label :for="field.name">{{ field.label }}</label>
             <input v-if="field.type === 'text' || field.type === 'password' || field.type === 'email'"
-                v-model="formData[field.name]"  :type="field.type" :name="field.name" :id="field.name" required />
+                v-model="formData[field.name]"  :type="field.type" :name="field.name" :id="field.name" 
+                @blur="validateField(field)" required />
+            <span v-if="errors[field.name]" class="error-message">
+                <i class="warning-icon">⚠️</i> {{ errors[field.name] }}
+            </span>
 
             <select v-if="field.type === 'select'" v-model="formData[field.name]" :name="field.name" :id="field.name" 
                 required>
@@ -11,16 +15,13 @@
                     {{ option.label }}
                 </option>
             </select>
-
         </div>
         <button class="rounded-button" type="submit">{{ submitButtonText }}</button>
-
     </form>
-
 </template>
 
 <script setup>
-import { ref,watch } from 'vue';
+import { ref, watch } from 'vue';
 
 const props = defineProps({
     fields: {
@@ -39,11 +40,10 @@ const props = defineProps({
 
 // Initialize formData based on the fields prop
 const formData = ref({});
+const errors = ref({});
 
 // Function to initialize or update formData
 const initializeFormData = () => {
-    console.log(props.fields);
-    
     formData.value = props.fields.reduce((acc, field) => {
         acc[field.name] = field.value || ''; // Use field.value if provided, otherwise default to ''
         return acc;
@@ -58,18 +58,54 @@ watch(() => props.fields, () => {
     initializeFormData();
 }, { deep: true });
 
-//Simply put the handleSubmit to the given function
-const handleSubmit = () => {
-    props.onSubmit(formData.value);
+// Validation methods
+const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
 };
 
+const validatePassword = (password) => {
+    return password.length >= 8;
+};
+
+const validateRequiredField = (field) => {
+    const value = formData.value[field.name]?.trim();
+    if (!value) {
+        errors.value[field.name] = `${field.label} es requerido`;
+    } else {
+        errors.value[field.name] = '';
+    }
+};
+
+const validateField = (field) => {
+    validateRequiredField(field);
+    if (field.type === 'email' && !validateEmail(formData.value[field.name])) {
+        errors.value[field.name] = 'La dirección email es invalida';
+    } else if (field.type === 'password' && !validatePassword(formData.value[field.name])) {
+        errors.value[field.name] = 'La contraseña debe tener al menos 8 caracteres';
+    }
+};
+
+// Handle form submission
+const handleSubmit = () => {
+    let valid = true;
+    props.fields.forEach(field => {
+        validateField(field);
+        if (errors.value[field.name]) {
+            valid = false;
+        }
+    });
+
+    if (valid) {
+        props.onSubmit(formData.value);
+    }
+};
 </script>
 
 <style scoped>
 form {
     display: grid;
     grid-template-columns: 1fr;
-    /* Default to one column layout */
     padding: 20px;
 }
 
@@ -99,5 +135,23 @@ input:focus,
 select:focus {
     border: 1.5px solid var(--main-blue);
     outline: none;
+}
+
+.error-message {
+    color: #d32f2f;
+    font-size: 14px;
+    margin-top: 5px;
+    display: flex;
+    align-items: center;
+    background-color: #ffebee;
+    padding: 10px;
+    border-radius: 8px;
+    border: 1px solid #d32f2f;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.warning-icon {
+    margin-right: 8px;
+    font-size: 18px;
 }
 </style>
